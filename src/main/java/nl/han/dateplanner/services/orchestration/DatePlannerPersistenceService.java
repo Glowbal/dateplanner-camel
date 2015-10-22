8503e8863ab4f070b4b054f80a3ca83e9a35d7b4
Glowbal <thomasskooi@live.nl>
2015-10-22 13:29:38 +0200

    Removed unnecessary comments

--- src/main/java/nl/han/dateplanner/services/orchestration/DatePlannerPersistenceService.java
+++ src/main/java/nl/han/dateplanner/services/orchestration/DatePlannerPersistenceService.java
@@ -1,19 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package nl.han.dateplanner.services.orchestration;
 
 import nl.han.dateplanner.DatePlannerResponse;
@@ -24,14 +8,6 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 
-/**DatePlannerPersistenceService
- * When you run this route, messages can be obtained from two sources:
- * - inbox-folder (located in the bin-directory of your Tomcat/Jetty instance (don't run this example with mvn tomcat:run,
- * instead use a separate server. In src/main/webapp/WEB-INF/applyregistrationservice an example messages is included which
- * you can copy to your inbox folder in case your webservice is not available.
- * - a webservice with its wsdl served at http://localhost:8080/Dare2DateCamel/applyregistration.wsdl
- * (otherwise: http://localhost:8080/applyregistration.wsdl)
- */
 public class DatePlannerPersistenceService extends RouteBuilder {
 
     public DatePlannerPersistenceService() {
--- src/main/java/nl/han/dateplanner/services/orchestration/DatePlannerService.java
+++ src/main/java/nl/han/dateplanner/services/orchestration/DatePlannerService.java
@@ -1,41 +1,13 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package nl.han.dateplanner.services.orchestration;
 
 import nl.han.dateplanner.DatePlannerRequest;
 import nl.han.dateplanner.DatePlannerResponse;
-import nl.han.dateplanner.services.business.DateTaskFactory;
 import nl.han.dateplanner.services.business.IDateTaskService;
 import org.apache.camel.*;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.spi.EventNotifier;
 
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-
-/**
- * When you run this route, messages can be obtained from two sources:
- * - inbox-folder (located in the bin-directory of your Tomcat/Jetty instance (don't run this example with mvn tomcat:run,
- * instead use a separate server. In src/main/webapp/WEB-INF/applyregistrationservice an example messages is included which
- * you can copy to your inbox folder in case your webservice is not available.
- * - a webservice with its wsdl served at http://localhost:8080/Dare2DateCamel/applyregistration.wsdl
- * (otherwise: http://localhost:8080/applyregistration.wsdl)
- */
 public class DatePlannerService extends RouteBuilder {
 
     IDateTaskService taskService;
